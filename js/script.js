5a8a5d22233a91e413cc3724607456b5ae3e7e01
Artem <shariukov.artem@mail.ru>
2018-06-04 00:32:28 +0300

    sticky header, added Pelevin and Zimmel, restructured scss

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 console.log('Hello my dear friend! You can get even better.');
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+    contentPaddingTop()
+};
+
+// Get the header
+var content = document.getElementById("content");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function contentPaddingTop() {
+    if (window.pageYOffset >= sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
